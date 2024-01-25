8c31a9d2f16234fa89535ee022c7a0e6fb6f1f44
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-01-25 20:43:22 +0100

    end

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+const Name = prompt("Name")  ;
+const Surname = prompt("Surname");
+const Color = prompt("Favourite Color");
+const getNumber = Math.floor(Math.random() * 100)
+
+
+
+
+
+document.getElementById("password").innerHTML =`${Name}${Surname}${Color}${getNumber}`;
+
+
+
